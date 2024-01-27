73c4f19579b7bfd87b71645390b608b7bbe338b8
RoDoCode <>
2024-01-27 14:02:59 +0000

    Wire up contact form

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Contact
 from .forms import CollaborateForm
 
 
 def contact_us(request):
     """
-    Renders the About page
+    Renders the Contact page
     """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration \
+                                 request received! I endeavour to respond \
+                                 within 2 working days.")
+
     contact = Contact.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
