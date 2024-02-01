80eb9d68c4fc3e1be2227dba8cc9de37067cdb07
RoDoCode <>
2024-02-01 23:44:38 +0000

    Fix admin.py by removing non-functional fields with ChefProfileAdmin class

--- chefprofile/admin.py
+++ chefprofile/admin.py
@@ -7,9 +7,4 @@ from django_summernote.admin import SummernoteModelAdmin
 class ChefProfileAdmin(SummernoteModelAdmin):
 
     search_fields = ['user', 'bio', 'location', 'birth_date',]
-    list_filter = ('created_on')
-    prepopulated_fields = {'user': ('user',)}
     summernote_fields = ('bio',)
-
-
-
