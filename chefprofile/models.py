595c622d60b42895fc83122f644ad9532aabf2a0
RoDoCode <>
2024-02-03 18:19:47 +0000

    Update chefprofile model to include null=true to attempt fix for view_chefprofile.html load issue

--- chefprofile/models.py
+++ chefprofile/models.py
@@ -9,13 +9,11 @@ from django.db.models.signals import post_save
 
 class ChefProfile(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name="chefprofile")
+        User, null=True, on_delete=models.CASCADE, related_name="chefprofile")
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     profile_pic = CloudinaryField('image', default='placeholder')
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            ChefProfile.objects.create(user=instance)
+    def __str__(self):
+        return str(self.user)
