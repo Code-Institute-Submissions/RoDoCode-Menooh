437177b848ae28ebad7e60f7a4b02dc1831e3050
RoDoCode <>
2024-02-04 14:55:32 +0000

    Update pagination settings to 14 items per page

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ from .forms import CommentForm
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 50
+    paginate_by = 14
 
 
 def post_detail(request, slug):
