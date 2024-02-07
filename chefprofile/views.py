c3243eabb40536617c0138fcdad95762a1ea7b67
RoDoCode <>
2024-02-07 15:12:35 +0000

    Fix missing httpresponse for if/else statement on chefprofile.view.py

--- chefprofile/views.py
+++ chefprofile/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import ChefProfile
 from .forms import ChefProfileForm
+from django.contrib import messages
 
 
 def edit_chefprofile(request):
     chefprofile = get_object_or_404(ChefProfile, user=request.user)
 
     if request.method == 'POST':
-        chef_form = ChefProfileForm(data=request.POST, instance=chefprofile)
+        chef_form = ChefProfileForm(request.POST, instance=chefprofile)
         if chef_form.is_valid():
             chef_form.save()
-            messages.add_message(request, messages.SUCCESS, "Chef Profile \
-                                 Updated")
-            return redirect('chefprofile')
-
+            messages.success(request, 'Profile updated successfully')
+            return render(request, 'chefprofile/view_chefprofile.html', {'chefprofile': chefprofile, })
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+            return render(request, 'chefprofile/edit_chefprofile.html', {'chef_form': chef_form})
     else:
         chef_form = ChefProfileForm(instance=chefprofile)
         return render(request, 'chefprofile/edit_chefprofile.html', {'chef_form': chef_form})
