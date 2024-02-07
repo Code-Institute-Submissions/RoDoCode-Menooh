b23ea5ca638313033550d8302d20de6750a26411
RoDoCode <>
2024-02-07 16:17:40 +0000

    Add socials to chef-profile form

--- chefprofile/forms.py
+++ chefprofile/forms.py
@@ -5,4 +5,5 @@ from .models import ChefProfile
 class ChefProfileForm(forms.ModelForm):
     class Meta:
         model = ChefProfile
-        fields = ['bio', 'location', 'birth_date', 'profile_pic']
+        fields = ['bio', 'location', 'birth_date', 'profile_pic', 'facebook_url',
+                  'twitter_url', 'instagram_url', 'pinterest_url', 'tiktok_url', 'website_url']
