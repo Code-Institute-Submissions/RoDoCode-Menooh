6e822c14b6536268eccbaf18347ea52481ee64cc
RoDoCode <>
2024-02-11 13:53:37 +0000

    Add cookbook model to models.py in blog

--- blog/models.py
+++ blog/models.py
@@ -54,3 +54,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+
+
+class Cookbook(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    cover_image = CloudinaryField('image', default='placeholder')
+    description = models.TextField((max_length=500)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    dishes = models.ManyToManyField(Post, related_name='books')
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | collected by {self.author}"
